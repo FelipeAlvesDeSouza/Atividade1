90e6f183e2d6f886bcd03289c10d370728f41301
Felipe Alves <felipealvess_hpg_@hotmail.com>
2023-04-03 22:48:48 -0300

    Atividade Feita

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Header } from "./components/Header"
+
+export function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export function Header(){
+    return (
+        <header>
+            <h1>Sou o cabeçalho</h1>
+            <p>Parágrafo pra dar uma incrementada mais</p>
+        </header>
+    )
+}
